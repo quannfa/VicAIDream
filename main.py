d87f412a33012d91527a85ad680e20665738d55b
quanfa <quanfa@tju.edu.cn>
2025-02-07 18:48:12 +0800

    配置和输出环境

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 #%% init project env
 import EsyPro
-EsyPro.Project.from_main_file(__file__).create_structure()
+sfs = EsyPro.ScriptResultManager('zqf',locals(), version=0)
 
+def output_env():
+    pre_fix = sfs.package_path.cat('.condaenv')
+    import os
+    os.system(
+        f"conda env export > environment.yml"
+    )
+
+
+#%% main
+if __name__ == '__main__':
+    print(f'start {__file__}')
+
+    output_env()
+    
+    #%% end script
+    print(f'end {__file__}')
